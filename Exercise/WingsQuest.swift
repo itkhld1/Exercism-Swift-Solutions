137153f1e0146e2a8208e6a03738062b1571cfdd
khlaid samim <95288187+itkhld@users.noreply.github.com>
2024-02-28 16:15:02 +0300

    create WingsQuest.swift

---
+++ Exercise/WingsQuest.swift
@@ -0,0 +1,23 @@
+func bonusPoints(powerUpActive: Bool, touchingEagle: Bool) -> Bool {
+    if powerUpActive && touchingEagle {
+                                        return true
+    } else {
+           return false
+    }
+}
+
+func score(touchingPowerUp: Bool, touchingSeed: Bool) -> Bool {
+  if touchingPowerUp || touchingSeed {
+                                      return true
+  } else{
+      return false
+  }
+}
+
+func lose(powerUpActive: Bool, touchingEagle: Bool) -> Bool {
+  return !powerUpActive && touchingEagle
+}
+
+func win(hasPickedUpAllSeeds: Bool, powerUpActive: Bool, touchingEagle: Bool) -> Bool {
+    return hasPickedUpAllSeeds && !lose(powerUpActive: powerUpActive, touchingEagle: touchingEagle)
+}
